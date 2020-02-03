82aa51213936663d92edeebf35b493d50a9ac079
Guillermo Rebaza <grebaza@gmail.com>
2020-02-03 16:00:03 -0500

    new image for zookeeper 3.5.6

--- kafka/img-build/zookeeper/docker-entrypoint.sh
+++ kafka/img-build/zookeeper/docker-entrypoint.sh
@@ -1,85 +1,47 @@
 #!/bin/bash
 
-# Exit immediately if a *pipeline* returns a non-zero status. (Add -x for command tracing)
 set -e
 
-if [[ -z $1 ]]; then
-    ARG1="start"
-else
-    ARG1=$1
+# Allow the container to be started with `--user`
+if [[ "$1" = 'zkServer.sh' && "$(id -u)" = '0' ]]; then
+    chown -R zookeeper "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR" "$ZOO_CONF_DIR"
+    exec gosu zookeeper "$0" "$@"
 fi
 
-if [[ -n "$JMXPORT" ]]; then
-    # Docker requires extra JMX-related JVM flags beyond what Zookeeper normally uses
-    JMX_EXTRA_FLAGS="-Djava.rmi.server.hostname=${JMXHOST} -Dcom.sun.management.jmxremote.rmi.port=${JMXPORT} -Dcom.sun.management.jmxremote.port=${JMXPORT}"
-    if [[ -n "$JVMFLAGS" ]]; then
-        export JVMFLAGS="${JMX_EXTRA_FLAGS} ${JVMFLAGS} "
-    else
-        export JVMFLAGS="${JMX_EXTRA_FLAGS} "
+# Generate the config only if it doesn't exist
+if [[ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]]; then
+    CONFIG="$ZOO_CONF_DIR/zoo.cfg"
+    {
+        echo "dataDir=$ZOO_DATA_DIR"
+        echo "dataLogDir=$ZOO_DATA_LOG_DIR"
+
+        echo "tickTime=$ZOO_TICK_TIME"
+        echo "initLimit=$ZOO_INIT_LIMIT"
+        echo "syncLimit=$ZOO_SYNC_LIMIT"
+
+        echo "autopurge.snapRetainCount=$ZOO_AUTOPURGE_SNAPRETAINCOUNT"
+        echo "autopurge.purgeInterval=$ZOO_AUTOPURGE_PURGEINTERVAL"
+        echo "maxClientCnxns=$ZOO_MAX_CLIENT_CNXNS"
+        echo "standaloneEnabled=$ZOO_STANDALONE_ENABLED"
+        echo "admin.enableServer=$ZOO_ADMINSERVER_ENABLED"
+    } >> "$CONFIG"
+    if [[ -z $ZOO_SERVERS ]]; then
+      ZOO_SERVERS="server.1=localhost:2888:3888;2181"
     fi
+
+    for server in $ZOO_SERVERS; do
+        echo "$server" >> "$CONFIG"
+    done
+
+    if [[ -n $ZOO_4LW_COMMANDS_WHITELIST ]]; then
+        echo "4lw.commands.whitelist=$ZOO_4LW_COMMANDS_WHITELIST" >> "$CONFIG"
+    fi
+
 fi
 
-# Process some known arguments to run Zookeeper ...
-case $ARG1 in
-    start)
-        # Copy config files if not provided in volume
-        cp -r $ZK_HOME/conf.orig/* $ZK_HOME/conf
+# Write myid only if it doesn't exist
+if [[ ! -f "$ZOO_DATA_DIR/myid" ]]; then
+    echo "${ZOO_MY_ID:-1}" > "$ZOO_DATA_DIR/myid"
+fi
 
-        #
-        # Process the logging-related environment variables. Zookeeper's log configuration allows *some* variables to be
-        # set via environment variables, and more via system properties (e.g., "-Dzookeeper.console.threshold=INFO").
-        # However, in the interest of keeping things straightforward and in the spirit of the immutable image,
-        # we don't use these and instead directly modify the Log4J configuration file (replacing the variables).
-        #
-        if [[ -z "$LOG_LEVEL" ]]; then
-            LOG_LEVEL="INFO"
-        fi
-        sed -i -r -e "s|\\$\\{zookeeper.root.logger\\}|$LOG_LEVEL, CONSOLE|g" $ZK_HOME/conf/log4j.properties
-        sed -i -r -e "s|\\$\\{zookeeper.console.threshold\\}|$LOG_LEVEL|g" $ZK_HOME/conf/log4j.properties
-
-        #
-        # Configure cluster settings
-        #
-        if [[ -z "$SERVER_ID" ]]; then
-            SERVER_ID="1"
-        fi
-        if [[ -z "$SERVER_COUNT" ]]; then
-            SERVER_COUNT=1
-        fi
-        if [[ $SERVER_ID = "1" ]] && [[ $SERVER_COUNT = "1" ]]; then
-            echo "Starting up in standalone mode"
-        else
-            echo "Starting up ${SERVER_ID} of ${SERVER_COUNT}"
-            #
-            # Append the server addresses to the configuration file ...
-            #
-            echo "" >> $ZK_HOME/conf/zoo.cfg
-            echo "#Server List" >> $ZK_HOME/conf/zoo.cfg
-            for i in $( eval echo {1..$SERVER_COUNT});do
-                if [ "$SERVER_ID" = "$i" ];then
-                    echo "server.$i=0.0.0.0:2888:3888" >> $ZK_HOME/conf/zoo.cfg
-                else
-                    echo "server.$i=zookeeper-$i:2888:3888" >> $ZK_HOME/conf/zoo.cfg
-                fi
-            done
-            #
-            # Persists the ID of the current instance of Zookeeper in the 'myid' file
-            #
-            echo ${SERVER_ID} > $ZK_HOME/data/myid
-        fi
-
-        # Now start the Zookeeper server
-        export ZOOCFGDIR="$ZK_HOME/conf"
-        export ZOOCFG="zoo.cfg"
-        exec $ZK_HOME/bin/zkServer.sh start-foreground
-        ;;
-    status)
-        exec $ZK_HOME/bin/zkServer.sh status
-        ;;
-    cli)
-        exec "$ZK_HOME/bin/zkCli.sh -server 0.0.0.0:2181"
-        ;;
-esac
-
-# Otherwise just run the specified command
 exec "$@"
