45e1fe3c9f1d319126bada45a67d1ba70fe9d511
Guillermo Rebaza <grebaza@gmail.com>
2020-06-08 16:45:16 -0500

    add confluent-hub download script

---
+++ kafka-connect/img-build/connect/docker-chub-download.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+
+CHUB_URI=https://api.hub.confluent.io/api/plugins/confluentinc
+CHUB_PKG="$1"
+CHUB_PKG_VERSION="$2"
+CHUB_URI="$CHUB_URI/$CHUB_PKG/versions/$CHUB_PKG_VERSION"
+CHUB_FILE=$(mktemp)
+curl -fSL -o "$CHUB_FILE" \
+  $( curl --stderr /dev/null "$CHUB_URI" \
+    | sed -rn \
+    's/.*("archive":.*)/\1/; s/(("url":"([^"]*)".*)*.)*/\3/p' \
+  )
+mv "$CHUB_FILE" "$CHUB_DEP_DESTINATION"
+unzip "$CHUB_DEP_DESTINATION/$CHUB_FILE"
+rm "$CHUB_DEP_DESTINATION/$CHUB_FILE"
+
